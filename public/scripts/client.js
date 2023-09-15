843b67a19901ed34e6a850e36048b6c190594d15
linkwithlionhart <linkwithlionhart@gmail.com>
2023-09-15 01:16:28 -0700

    Implement feature to fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,45 +6,6 @@
 
 $(document).ready(() => {
   
-  // Add event listener and prevent default behavior.
-  $(".new-tweet form").submit(function(event) {
-    event.preventDefault();
-    
-    // Serialize form data.
-    const serializedData = $(this).serialize();
-
-    // Use jQuery to make AJAX post request.
-    $.post('/tweets/', serializedData, function(response) {
-      console.log(response);
-    });
-  }); 
-
-  // TEMPORARY: hard coded test data:
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   /**
    * Creates a tweet element for provided tweet data.
    * @param {Array} tweets - Array of tweet objects for rendering. 
@@ -73,7 +34,6 @@ $(document).ready(() => {
     $tweet.html(tweetHTML);
     
     return $tweet;
-
   }
 
   /**
@@ -91,7 +51,27 @@ $(document).ready(() => {
     }
   }
 
-  renderTweets(data);
+  // Fetch tweets from the server and render to page.
+  const loadTweets = () => {
+    $.ajax('/tweets', { method: 'GET'})
+    .then(function(tweets) {
+      renderTweets(tweets);
+    })
+  }
+
+  // Handle the form submission.
+  $(".new-tweet form").submit(function(event) {
+    event.preventDefault();
+    
+    // Serialize form data.
+    const serializedData = $(this).serialize();
+
+    // Use jQuery to make AJAX post request.
+    $.post('/tweets/', serializedData, function(response) {
+      console.log(response);
+      loadTweets(); 
+    });
+  }); 
 
 });
 
